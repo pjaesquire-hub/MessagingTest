622226e0eaaaf864ef6d74194f6e84f8c2e891c3
phil <224983187+pjaesquire-hub@users.noreply.github.com>
2026-01-21 12:32:33 +0000

    updated script for testing staging

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const button = document.getElementById("openChatBtn");
 
-        button.addEventListener('click', function(){
-            const name = document.getElementById("name").value.trim();
-            const product = document.getElementById("product").value.trim()
-            const issue = document.getElementById("issue").value.trim()
-            
-           
-               if (!name) { alert("Please enter your name"); return; }
-   if (!product) { alert("Please select a product"); return; }
-   if (!issue) { alert("Please describe your issue"); return; }
+    button.addEventListener('click', function(){
+        const name = document.getElementById("name").value.trim();
+        const product = document.getElementById("product").value.trim();
+        const issue = document.getElementById("issue").value.trim();
 
-            
-            window.sprChat('openNewConversation', {
-                        initialMessages: [{message: issue, isSentByUser:true}],  
-                        conversationContext:{"_c_6761b89040a38a010e72e3ab": product, "_c_65cb8fb5aa6941089495b8ad": name}});
-        
+  
+        // if (!name) { alert("Please enter your name"); return; }
+        // if (!product) { alert("Please select a product"); return; }
+        // if (!issue) { alert("Please describe your issue"); return; }
+
+        window.sprChat('openNewConversation', {
+            initialMessages: [{message: issue, isSentByUser:true}],  
+            conversationContext: {
+               "_c_66599c9e65546f03da49e758": ['EE App - CheckLatestEeMobileBill']
+            }
         });
-            
+    });
+
+
